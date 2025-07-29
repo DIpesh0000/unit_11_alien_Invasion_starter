c9ec6f362aeadc2c85329c7f95434dbbd2cd02ad
DIpesh0000 <84489440+DIpesh0000@users.noreply.github.com>
2025-07-29 02:06:01 -0400

    changed images

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,12 @@
+"""
+Program Name: Lab11_CustomAssets
+Author: Dipesh Dahal
+Purpose: Replace default spaceship game assets with custom images and sounds
+Date: 07/29/2025
+"""
+
 import sys
 import pygame
 
 if __name__ == "__main__":
-    """Test comment"""
     pass
