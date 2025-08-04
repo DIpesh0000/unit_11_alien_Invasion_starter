ca26eb022a12c3c51e33b875244c78ef9be3787a
DIpesh0000 <84489440+DIpesh0000@users.noreply.github.com>
2025-08-04 19:14:49 -0400

    created alien file update settings

--- settings.py
+++ settings.py
@@ -7,15 +7,21 @@ class settings:
         self.screen_w = 1200
         self.screen_h = 800
         self.FPS = 60
-        self.bg_file = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'images' / 'summer5.png'
-        self.ship_file = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'images' / 'newship_2_nobg.png'
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'summer5.png'
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'newship_2_nobg.png'
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 5
 
-        self.bullet_file = Path.cwd() / 'unit_11_alien_Invasion_starter' / 'Assets' / 'images' / 'beam.png'
-        self.laser_sound = Path.cwd() /  'unit_11_alien_Invasion_starter' / 'Assets' / 'sound' / 'laser.mp3'
+        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'beam.png'
+        self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.bullet_speed = 7 
         self.bullet_w = 25
         self.bullet_h = 80
         self.bullet_amount = 5
+
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+        self.fleet_speed = 5
+        
