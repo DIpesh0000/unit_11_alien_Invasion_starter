ba5bbbf4df7182108e3f02de7c5ecd1576824d2e
DIpesh0000 <84489440+DIpesh0000@users.noreply.github.com>
2025-08-10 21:48:03 -0400

    set up button to start game and make mouse disappear

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,7 @@ from ship import Ship
 from arsenal import Arsenal
 from alien_fleet import AlienFleet
 from time import sleep
+from button import Button
 
 class AlienInvasion:
 
@@ -39,7 +40,8 @@ class AlienInvasion:
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self)
         self.alien_fleet.create_fleet()
-        self.game_active = True
+        self.play_button = Button(self,'Play')
+        self.game_active = False
 
     def run_game(self):
         while self.running:
@@ -82,11 +84,24 @@ class AlienInvasion:
         self.ship.arsenal.arsenal.empty()
         self.alien_fleet.fleet.empty()
         self.alien_fleet.create_fleet()
+    def restart_game(self):
+        # setting up dynamic settings
+        #reset game stats
+        #update HUD scores
+        self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
+
 
     def _update_screen(self):
         self.screen.blit(self.bg,(0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
         pygame.display.flip()
 
     def _check_events(self):
@@ -99,6 +114,13 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                  self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game()
                  
     def _check_keyup_events(self,event):
         if event.key == pygame.K_RIGHT:
